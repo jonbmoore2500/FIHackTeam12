1ff485588108184dbfb490a32fc61086504c2695
+ <familymanjosh@gmail.com>
2023-09-26 10:13:21 -0700

    deleting folder

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from FIHackTeam12.backend.config import db
 
 # Models go here!
--- backend/seed.py
+++ backend/seed.py
@@ -7,8 +7,8 @@ from random import randint, choice as rc
 from faker import Faker
 
 # Local imports
-from app import app
-from models import db
+from FIHackTeam12.backend.app import app
+from FIHackTeam12.backend.models import db
 
 if __name__ == '__main__':
     fake = Faker()
