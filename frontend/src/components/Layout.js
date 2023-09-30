7a666aa76ca9d3856e07023d8b45e66b0787a1e5
Jonathan <jonathanmoorevla@gmail.com>
2023-09-30 16:04:37 -0500

    Format user profile and modify profile form

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -4,16 +4,20 @@ import {Outlet, NavLink} from "react-router-dom"
 function Layout() {
 
     return (
-        <div>
-            <div id="navHeader">
-                <NavLink to="/" >Profile</NavLink>
-                <NavLink to="/portfolio" >Portfolio</NavLink>
-                <NavLink to="/docModifier" >Modify Center</NavLink>
-            </div>
+        <>
+            <nav id="navHeader">
+                <ul>
+                    <li><NavLink to="/" >Profile</NavLink></li>
+                    <li><NavLink to="/portfolio" >Portfolio</NavLink></li>
+                    <li><NavLink to="/docModifier" >Modify</NavLink></li>
+                </ul>
+            </nav>
+
             <div id="appContent">
                 <Outlet />
             </div>
-        </div>
+        </>
+
     )
 }
 
